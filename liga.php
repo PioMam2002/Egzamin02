503a09d3f5bcd80afa75ecb0e5742a963299c436
unknown <Nemczak@yahoo.com>
2021-10-21 12:43:41 +0200

    aktualizacja

---
+++ liga.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>piłka nożna</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+
+<body>
+    <header>
+        <h3>Reprezentacja Polski w Piłce Nożnej</h3>
+        <img src="obraz1.jpg" alt="reprezentacja">
+    </header>
+    <div id="podzial">
+        <div id="lewy">
+            <form action="liga.php" method="post">
+            
+            </form>
+            <img src="zad2.png" alt="piłka">
+            <p>Autor:Piotr Mamiński</p>
+        </div>
+        <div id="prawy">
+        
+        </div>
+    </div>
+    <main>
+        <h3>Liga mistrzów</h3>
+    </main>
+    <div id="liga">
+
+        
+    </div>
+
+</body>
+
+</html>
